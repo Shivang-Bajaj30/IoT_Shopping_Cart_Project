b5ec7607d453e396b620b4502f94f47c0d4ee676
sudipkundu <sudip.kundu@bcah.christuniversity.in>
2026-04-14 17:43:35 +0530

    kam hogeya bete

--- testDb.js
+++ testDb.js
@@ -1,15 +1,16 @@
+import "dotenv/config";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { getDatabase, ref, get } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAu-_8iz-3iSPdnimLq-PRbSsj323KZYwc",
-  authDomain: "smartcart-a5e29.firebaseapp.com",
-  databaseURL: "https://smartcart-a5e29-default-rtdb.firebaseio.com",
-  projectId: "smartcart-a5e29",
-  storageBucket: "smartcart-a5e29.appspot.com",
-  messagingSenderId: "521016468149",
-  appId: "1:521016468149:web:f4d7395f2f6baa741dd4fc",
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
